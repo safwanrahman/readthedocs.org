7883d98b096f5ffa6879a4eab2bf7cf83ab39af5
Safwan Rahman <safwan.rahman15@gmail.com>
2018-05-18 08:51:18 +0600

    Dd

--- readthedocs/doc_builder/environments.py
+++ readthedocs/doc_builder/environments.py
@@ -275,7 +275,7 @@ class DockerBuildCommand(BuildCommand):
         prefix = ''
         if self.bin_path:
             prefix += 'PATH={0}:$PATH '.format(self.bin_path)
-        return ("/bin/sh -c 'cd {cwd} {prefix}{cmd}'"
+        return ("/bin/sh -c 'cd {cwd} && {prefix}{cmd}'"
                 .format(
                     cwd=self.cwd,
                     prefix=prefix,
